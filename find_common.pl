257b550babca388adac2e9439be82d217ff6c0f0
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2018-05-14 06:49:28 -0400

    Update find_common.pl

--- find_common.pl
+++ find_common.pl
@@ -5,7 +5,9 @@ use strict;
 my (	$mit_content	,
 	$chl_content	,
 	@mit_name	,
-	@chl_name	); 
+	@chl_name	,
+	@mit_cname	,
+	@chl_cname	); 
 
 
 chdir "$ARGV[0]" or die "Can't chdir to $ARGV[0]: $!";
@@ -19,16 +21,21 @@ $chl_content = `find . -mindepth 1 -maxdepth 1 -type d `;
 @chl_name = split /\n/, $chl_content;
 
 
-foreach my $mit (sort @mit_name){
-	$mit =~ s/\A\.\/(\w+)/$1/;
-	$mit =~ s/([a-zA-Z])([a-zA-Z])[a-zA-Z]+_(\w+)/$1$2_$3/ if ( $mit =~ m/_/ );
+foreach my $i (0..$#mit_name){
+	$mit_cname[$i] = $1 if $mit_name[$i] =~ m/\A\.\/(\w+)/;
+	$mit_cname[$i] =~ s/([a-zA-Z])([a-zA-Z])[a-zA-Z]+_(\w+)/$1$2_$3/ if ( $mit_cname[$i] =~ m/_/ );
+	#print "$mit_cname[$i]\n"
+		
 }
 
 
-foreach my $chl (sort @chl_name){
-	$chl =~ s/\A\.\/(\w+)/$1/;
-	$chl =~ s/([a-zA-Z])([a-zA-Z])[a-zA-Z]+_(\w+)/$1$2_$3/ if ( $chl =~ m/_/ );
-	foreach my $mit (@mit_name){
-		print "$chl\n" if $mit eq $chl;
+foreach my $i (0..$#chl_name){
+	$chl_cname[$i] = $1 if $chl_name[$i] =~ m/\A\.\/(\w+)/;
+	$chl_cname[$i] =~ s/([a-zA-Z])([a-zA-Z])[a-zA-Z]+_(\w+)/$1$2_$3/ if ( $chl_cname[$i] =~ m/_/ );
+		
+	foreach my $mit (@mit_cname){
+		print "$chl_cname[$i]\n" if $mit eq $chl_cname[$i];
 	}
 }
+
+
