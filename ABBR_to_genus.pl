37880a4ddf2257fa6071d1df60cb14142dcd073f
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2018-05-14 20:53:24 -0400

    Create ABBR_to_genus.pl

---
+++ ABBR_to_genus.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+use strict;
+
+my(@abbr, @ge);
+
+
+open FI , '<' , $ARGV[0];
+open SI , '<' , $ARGV[1];
+
+
+while (<SI>){
+	chomp;
+	my($ge , $abbr) = (split /,/)[4 , 9];
+	push @ge , $ge;
+	push @abbr , $abbr;
+}
+
+
+while (<FI>){
+	chomp;
+	my $sp = (split /,/)[0];
+	$sp =~ s/\A\.\///;
+	
+	for my $i (0..$#abbr){
+		if ($abbr[$i] eq $sp){
+			mkdir "$ge[$i]" , 0755 or warn "Cannot make directory: $!" unless (-e $ge[$i]);
+			system "cp -r ../genomes/$sp $ge[$i]/";
+		}
+	}
+
+}
